c53c2bda07e8235aa3954ac6f41b0a57ebd88188
agogte <agogte@gmu.edu>
2023-10-02 15:54:42 -0400

    v1.0

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange}/>
       {currentWeather && <CurrentWeather data={currentWeather}/>}
-      <Forecast />
+      {currentForecast && <Forecast data={currentForecast}/>}
     </div>
   );
 }
